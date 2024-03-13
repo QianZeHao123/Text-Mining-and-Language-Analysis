a8a5dc89fac4cb4a8468f287a3ac47a481ee04c2
Zehao Qian <qiazehao123@gmail.com>
2024-03-13 17:14:16 +0000

    .

---
+++ summative/index.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+import re
